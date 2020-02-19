b2c08b56b5ae07b08a20b5b2a86079add9fc1422
pedro1895 <pedro1895@gmail.com>
2020-02-18 21:47:09 -0300

    Please enter the commit message for your changes. Lines starting  with '#' will be ignored, and an empty message aborts the commit.
     On branch master
     Your branch is up to date with 'origin/master'.
    
     Changes to be committed:
    	modified:   package-lock.json
    	modified:   package.json
    	modified:   public/index.html
    	modified:   src/App.vue
    	new file:   src/assets/logo.svg
    	modified:   src/components/HelloWorld.vue
    	modified:   src/main.js
    	new file:   src/plugins/vuetify.js
    	modified:   vue.config.js

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,99 @@
 <template>
-  <div id="app">
-       Welcome to Your Vue.js App
-    <!--
-       <img alt="Vue logo" src="./assets/logo.png">
-      <HelloWorld msg="Welcome to Your Vue.js App"/>-->
-      <br/>
-      <input type="text" name="dns" id="dns" value="Type the dns"/>
-      <br/>
-      <input type="text" name="ip" id="ip" value="Type the ip"/> 
-      <br/>
-      <input type="button" v-on="click" value="enviar"/>
-  </div>
+  <v-app>
+    <v-app-bar
+      app
+      color="primary"
+      dark
+    >
+      <v-spacer></v-spacer>
+    </v-app-bar>
+
+    <v-content>
+  <v-card
+    class="mx-auto"
+    max-width="344"
+  >
+    <v-list-item two-line>
+      <v-list-item-content>
+        <v-list-item-title class="headline">DNS {{dns}}</v-list-item-title>
+        <v-list-item-subtitle>localhost {{ip}}</v-list-item-subtitle>
+      </v-list-item-content>
+    </v-list-item>
+
+    <v-card-text>
+      <v-form
+      ref="form"
+      v-model="valid"
+      :lazy-validation="lazy"
+    >
+      <v-text-field
+        v-model="dns"
+        :counter="15"
+        :rules="dnsRules"
+        label="Dns"
+        required
+        clearable
+      ></v-text-field>
+
+      <v-text-field
+        v-model="ip"
+        :counter="15"
+        :rules="ipRules"
+        label="IP"
+        required
+        clearable
+        v-mask="'##/##/####'"
+      ></v-text-field>
+
+      <v-btn
+        :disabled="!valid"
+        color="success"
+        class="mr-4"
+        @click="validate"
+      >
+       Alterar
+      </v-btn>
+
+      <v-btn
+        color="error"
+        class="mr-4"
+        @click="reset"
+      >
+       Limpar
+      </v-btn>
+       </v-form>
+      </v-card-text>
+    </v-card>
+    </v-content>
+  </v-app>
 </template>
 
 <script>
-//import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    //HelloWorld
-  }
-}
-</script>
+  data: () => ({
+      valid: true,
+      dns: '',
+      dnsRules: [
+        v => !!v || 'DNS is required',
+      ],
+      ip: '',
+      ipRules: [
+        v => !!v || 'Ip is required',
+      ],
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  }),
+  components: {
+  },
+  methods: {
+      validate () {
+        if (this.$refs.form.validate()) {
+          this.snackbar = true
+        }
+      },
+      reset () {
+        this.$refs.form.reset()
+      },
+    },
+};
+</script>
